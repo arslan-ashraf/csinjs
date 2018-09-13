cca9d56bf24d1830650646a4d684e538f32c80e2
Arslan Ashraf <arslanash@gmail.com>
2018-09-13 01:10:11 -0400

    added root html template and extensions

--- computer_science/urls.py
+++ computer_science/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from .views import home, categories, show, create
+from .views import home, categories, show, create, update, delete
 
 app_name = 'computer_science'
 
 urlpatterns = [
     url(r'^$', home, name = 'home'),
     url(r'^categories/$', categories, name = 'categories'),
-    url(r'^algorithm/title/(?P<friendly_title>[-\w]+)/$', show, name = 'title'),
+    url(r'^title/(?P<friendly_title>[-\w]+)/$', show, name = 'title'),
+	url(r'^title/(?P<friendly_title>[-\w]+)/edit/$', update, name = 'update'),
     url(r'^create/$', create, name = 'create'),
+    url(r'^delete/$', delete, name = 'delete'),
 ]
--- computer_science/views.py
+++ computer_science/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
 from .models import Algorithm
 from .forms import Algorithm_form
-
 
 	# print("-" * 50)
 	# print(form)
@@ -19,10 +18,24 @@ def create(request):
 		new_algorithm = form.save(commit = False)
 		print(new_algorithm.cleaned_date)
 		new_algorithm.save()
+		return redirect(new_algorithm, permanent = True)
 	items = { 'form': form }
 	return render(request, 'algorithms/algorithm_form.html', items)
 
+def update(request, friendly_title = None):
+	if not request.user.is_superuser:
+		return Http404
+	algorithm = get_object_or_404(Algorithm, friendly_title = friendly_title)
+	form = Algorithm_form(request.POST or None, instance = algorithm)
+	if form.is_valid():
+		updated_algorithm = form.save(commit = False)
+		updated_algorithm.save()
+		return redirect(updated_algorithm, permanent = True)
+	items = { 'form': form }
+	return render(request, 'algorithms/algorithm_form.html', items)	
+
 def categories(request):
+	all_categories = Algorithm.objects.all()
 	return
 
 def show(request, friendly_title = None):
@@ -37,3 +50,6 @@ def update(request, friendly_title = None):
 def delete(request, friendly_title = None):
 	if not request.user.is_superuser:
 		return Http404
+	algorithm = get_object_or_404(Algorithm, friendly_title = friendly_title)
+	algorithm.delete()
+	return redirect("computer_science:base", permanent = True)

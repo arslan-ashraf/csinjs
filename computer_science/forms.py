109ba22e08c093487457fccb8c66b29b9163c4f5
Arslan Ashraf <arslanash@gmail.com>
2018-09-13 22:43:45 -0400

    customized algorithm form, add css

--- computer_science/forms.py
+++ computer_science/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Algorithm
 
 class Algorithm_form(forms.ModelForm):
+	title = forms.CharField(widget = forms.TextInput(
+		attrs = { 'class': 'input-text', 'placeholder': 'Title' }))
+	category = forms.CharField(widget = forms.TextInput(
+		attrs = { 'class': 'algorithm-category', 'placeholder': 'Category' }))
+	code = forms.CharField(widget = forms.Textarea(
+		attrs = { 'class': 'code_for_algorithm', 'placeholder': 'Code', 'rows': 30, 'cols': 100 }))
 	class Meta:
 		model = Algorithm
 		fields = ['title', 'category', 'code']

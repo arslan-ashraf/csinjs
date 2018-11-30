de5e626e23623b072721cee489da9b938a78accf
Arslan Ashraf <arslanash@gmail.com>
2018-11-30 13:53:07 -0500

    committing old changes

--- csinjs/settings.py
+++ csinjs/settings.py
@@ -117,10 +117,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_files/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_files'),
 ]
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static_files')
+
+print("x" * 50)
+print(os.path.join(BASE_DIR, 'static_files'))
+print(STATIC_ROOT)
+print('x' * 50)

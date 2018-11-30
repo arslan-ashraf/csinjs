27b05185ad946a7d2ae0ff618c140a41ea424398
Arslan Ashraf <arslanash@gmail.com>
2018-11-30 15:43:42 -0500

    fixed a whole bunch of bugs

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
-from .views import blogs_home, create, edit, delete
+from .views import blogs_home, create, update, delete, show
 
 app_name = 'blog'
 
 urlpatterns = [
     url(r'^blogs/$', blogs_home, name = 'blogs_home'),
-    url(r'^blog/(?P<friendly_title>[-\w]+)/$', show, name = 'title'),
-	url(r'^blog/(?P<friendly_title>[-\w]+)/edit/$', update, name = 'update'),
+    url(r'^blog/(?P<friendly_title>[-\w]+)/$', show, name = 'show'),
+	url(r'^blog/(?P<friendly_title>[-\w]+)/update/$', update, name = 'update'),
 	url(r'^blog/(?P<friendly_title>[-\w]+)/delete/$', delete, name = 'delete'),
     url(r'^blog/create/$', create, name = 'create'),
    
--- computer_science/forms.py
+++ computer_science/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Algorithm
 
-class Blog_form(forms.ModelForm):
+class Algorithm_form(forms.ModelForm):
 	title = forms.CharField(widget = forms.TextInput(
 		attrs = { 'class': 'input-text', 'placeholder': 'Title' }))
 	category = forms.CharField(widget = forms.TextInput(

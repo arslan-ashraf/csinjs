b8a4bf1b1c172c88c90e1ebc492f93263ab37216
Arslan <arslanash@gmail.com>
2018-12-17 19:41:18 +0000

    finished ajax features

--- comments/admin.py
+++ comments/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from .models import Comment
+from .models import Comment, Notification
 
 class Comment_admin(admin.ModelAdmin):
 	list_display = ['user', 'article', 'algorithm', 'content', 'created_at']
 	class Meta:
 		model = Comment
 
+class Notification_admin(admin.ModelAdmin):
+	list_display = ['sender', 'receiver', 'action', 'body', 'url']
+	class Meta:
+		model = Notification
+
 admin.site.register(Comment, Comment_admin)
+admin.site.register(Notification, Notification_admin)
